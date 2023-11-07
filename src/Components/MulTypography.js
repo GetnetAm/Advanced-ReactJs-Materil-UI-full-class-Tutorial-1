2bfdd02045b4ad9905de440792725ee9293645f2
GetnetAm <getnetamsalu2119@gmail.com>
2023-11-07 11:00:13 -0800

    material ui class one

---
+++ src/Components/MulTypography.js
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+
+function MulTypography() {
+  return (
+    <div>
+        <Typography variant='h1' >H1 Heading </Typography>
+        <Typography variant='h2'>H2 Heading </Typography>
+        <Typography variant='h3'>H3 Heading </Typography>
+        <Typography variant='h4'>H4 Heading </Typography>
+        <Typography variant='h5'>H5 Heading </Typography>
+        <Typography variant='h6'>H6 Heading </Typography>
+      
+
+
+      
+    </div>
+  )
+}
+
+export default MulTypography
